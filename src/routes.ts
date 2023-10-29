111b0596cde725e459d734b7f96b7ca5c5a0c6be
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-29 21:39:27 +0800

    feat: run server at 0.0.0.0 instead of localhost

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import { Server } from "@hapi/hapi";
 import Joi from "joi";
-import JWT from 'jsonwebtoken';
+import JWT from "jsonwebtoken";
 
 export function routes(server: Server) {
   // Index route
@@ -8,18 +8,18 @@ export function routes(server: Server) {
     method: "GET",
     path: "/",
     options: {
-      tags: ['api'],
+      tags: ["api"],
       auth: false,
-      description: 'Index route',
+      description: "Index route",
       handler: (request, h) => {
-        h.state('hello', 'hapi');
-        request.logger.info('In handler %s', request.path);
+        h.state("hello", "hapi");
+        request.logger.info("In handler %s", request.path);
 
         return {
-          message: "Hello World!"
+          message: "Hello World!",
         };
-      }
-    }
+      },
+    },
   });
 
   // Hello route base on cookie
@@ -27,16 +27,16 @@ export function routes(server: Server) {
     method: "GET",
     path: "/hello",
     options: {
-      tags: ['api'],
+      tags: ["api"],
       auth: false,
       handler: (request, h) => {
         const helloMsg = request.state.hello;
-        request.logger.info('In handler %s', request.path);
+        request.logger.info("In handler %s", request.path);
         return {
-          message: `Hello ${helloMsg ?? 'IT Man'}`
+          message: `Hello ${helloMsg ?? "IT Man"}`,
         };
-      }
-    }
+      },
+    },
   });
 
   // Login route
@@ -44,62 +44,68 @@ export function routes(server: Server) {
     method: "POST",
     path: "/login",
     options: {
-      tags: ['api'],
+      tags: ["api"],
       auth: false,
       validate: {
         payload: Joi.object({
           username: Joi.string().required(),
-          password: Joi.string().required()
-        })
+          password: Joi.string().required(),
+        }),
       },
       handler: (request, h) => {
-        request.logger.info('In handler %s', request.path);
+        request.logger.info("In handler %s", request.path);
         const { username, password } = request.payload as {
           username: string;
-          password: string
+          password: string;
         };
         // Check if username and password match
-        if (username !== 'admin' || password !== 'admin') {
-          return h.response({ message: 'Invalid username or password' }).code(401);
+        if (username !== "admin" || password !== "admin") {
+          return h
+            .response({ message: "Invalid username or password" })
+            .code(401);
         }
 
         // Create JWT token
-        const secret = process.env.JWT_SECRET ?? 'YOUR_SECRET_KEY'
-        const token = JWT.sign({
-          username
-        }, secret);
+        const secret = process.env.JWT_SECRET ?? "YOUR_SECRET_KEY";
+        const token = JWT.sign(
+          {
+            username,
+          },
+          secret
+        );
 
         return {
           message: "Login successful",
-          token: token
+          token: token,
         };
-      }
-    }
+      },
+    },
   });
 
   server.route({
     method: "POST",
     path: "/todo",
     options: {
-      auth: 'jwt',
-      tags: ['api'],
-      description: 'Create todo',
+      auth: "jwt",
+      tags: ["api"],
+      description: "Create todo",
       validate: {
         payload: Joi.object({
-          title: Joi.string().required().min(5).messages({ message: 'Title should be at least 5 characters long' }),
+          title: Joi.string().required().min(5).messages({
+            message: "Title should be at least 5 characters long",
+          }),
           description: Joi.string().required(),
-          done: Joi.boolean().default(false)
-        })
+          done: Joi.boolean().default(false),
+        }),
       },
       handler: (request, h) => {
         const payload = request.payload as any;
-        request.logger.info('In handler %s', request.path);
+        request.logger.info("In handler %s", request.path);
         return {
           message: "Todo created",
-          data: payload
+          data: payload,
         };
       },
-    }
+    },
   });
-};
-
+}
